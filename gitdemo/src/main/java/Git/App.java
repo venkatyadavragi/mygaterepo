e5b1db7bc2127d3dbf15136ab6dab0aab025f94b
venkatyadavragi <48388786+venkatyadavragi@users.noreply.github.com>
2019-03-13 23:13:06 +0530

    Update App.java

--- gitdemo/src/main/java/Git/App.java
+++ gitdemo/src/main/java/Git/App.java
@@ -4,5 +4,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! This is venkat learning the git project");
+        System.out.println("I am in global repository");
     }
 }
