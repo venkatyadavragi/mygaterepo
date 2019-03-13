b4b7aa458273de0859e47dcee59185bc52664eb4
venkatyadavragi <venkatyadavragi@gmail.com>
2019-03-13 23:08:52 +0530

    new project created by venkat

---
+++ gitdemo/src/main/java/Git/App.java
@@ -0,0 +1,8 @@
+package com.git;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! This is venkat learning the git project");
+    }
+}
