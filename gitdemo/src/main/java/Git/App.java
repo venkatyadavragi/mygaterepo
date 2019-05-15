507ee36fb0ca0d18e8439ac753ba8a578458d4bf
venkatyadavragi <venkatyadavragi@gmail.com>
2019-05-15 12:48:30 +0530

    new file will be created

--- gitdemo/src/main/java/Git/App.java
+++
@@ -1,9 +0,0 @@
-package com.git;
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World! This is venkat learning the git project");
-        System.out.println("I am in global repository");
-    }
-}
